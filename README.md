7977e66a9eed7bcf486206a77d7de02ab513bb14
Roar Emaus <roar@emaus.no>
2021-04-14 12:43:50 +0200

    initial commit with tutorials so far

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rust language test and tutorials
---
+++ tutorial/primitives.rs
@@ -0,0 +1,18 @@
+
+fn main() {
+
+    let _logical: bool = true;
+
+    let _a_float: f64 = 1.0;
+    let _an_integer = 5i32;
+
+    let _default_float = 3.0;
+    let _default_integer = 7;
+
+    let mut _inferred_type = 12;
+    //mutable = 21;
+
+    //mutable = true;
+
+    let _mutable = true;
+}
